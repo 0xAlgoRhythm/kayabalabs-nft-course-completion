4255d6b6fe9192958f4318640a7ba68d7a25e3fd
John Okyere <112992966+mhiskall282@users.noreply.github.com>
2026-01-16 11:58:41 +0000

    Update KayabaCourseCompletionNFT.sol

--- src/KayabaCourseCompletionNFT.sol
+++ src/KayabaCourseCompletionNFT.sol
@@ -7,3 +7,12 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
+/**
+ * @title KayabaCourseCompletionNFT
+ * @dev NFT for Kayaba Labs Course Completion Certificates
+ * - Soulbound (non-transferable)
+ * - $0.50 minting fee
+ * - Stores only student ID, course name, and completion date
+ * - Batch minting available for owner
+ */
+
